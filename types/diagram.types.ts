84af996a0fddd7ecd51631ce0fafc7563017a884
DorinaFerencsik <36485244+DorinaFerencsik@users.noreply.github.com>
2020-11-14 15:02:01 +0100

    feat: diagram generation payloads, types etc, and string util

---
+++ types/diagram.types.ts
@@ -0,0 +1,9 @@
+/**
+ * Possible separator characters for csv files
+ */
+export type Separator = ',' | '|' | ';' | ' ';
+
+/**
+ * Available file formats for exporting diagrams
+ */
+export type Format = 'png' | 'pdf' | 'svg';
