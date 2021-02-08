c2bfa95ac139fa7a093c336a1953d28557917fd6
DorinaFerencsik <36485244+DorinaFerencsik@users.noreply.github.com>
2021-02-08 08:22:58 +0100

    fix: IGenerateDiagramPayload replace type with typeId

--- payloads/diagram/generate-diagram.interface.ts
+++ payloads/diagram/generate-diagram.interface.ts
@@ -1,10 +1,9 @@
 import { DiagramTypeEnum } from 'src/utils/enums/diagram/diagram-types.enum';
 import { Format } from 'src/utils/types/diagram.types';
-import { IDiagramParam } from 'utils/interfaces/diagram';
 
 export interface IGenerateDiagramPayload {
-  type: DiagramTypeEnum;
-  data: {source: string, name: string},
+  typeId: string;
+  data: {source: string, name: string};
   params: {
     [key: string]: any
   }[];
